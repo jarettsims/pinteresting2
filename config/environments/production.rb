1b8bb5bae1203e8371c7cc3447a139739f63ae40
Jarett Sims <jarett.sims@gmail.com>
2013-11-21 18:08:07 -0500

    Modified config/environments/production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Pinteresting2::Application.configure do
+Pinteresting::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -80,13 +80,13 @@ Pinteresting2::Application.configure do
 
   # Required for Heroku
   # Note to set this to your actual host
-  config.action_mailer.default_url_options = { :host => 'http://omr-jspinteresting.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'omr-pinteresting.com' }
 
   # Sets Paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET'],
+      :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
